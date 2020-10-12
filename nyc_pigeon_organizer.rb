93447deb639655d0b3851392049bf6b32ed379e8
Anber Jamshed <anberjam@yahoo.com>
2020-10-12 22:26:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_h = Hash.new 
+  data.each do |first_key, second_key|
+    second_key.each do |second_key, inner_array|
+      inner_array.each do |each_name|
+        if !new_h[each_name]
+          new_h[each_name]= Hash.new
+        if !new_h[each_name][first_key]
+          new_h[each_name][first_key]=Array.new
+  #binding.pry
 end
+end
+end
+end
+end
+
+data.each do |first_key, second_key|
+  second_key.each do |second_key, inner_array|
+    inner_array.each do |each_name|
+      new_h.each do |name, value|
+      if each_name==name && value==first_key
+        
+end
+end
+end
+end
+end
+binding.pry
+new_h
+end
+
